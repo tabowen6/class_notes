94ba96462aca97b165bcd9809dcb35d581b494a5
Taylor Bowen <bowen.taylor@TaylorBowen.lan1>
2021-03-25 20:39:29 -0400

    initialized class_notes repo

---
+++ day_2_notes.js
@@ -0,0 +1,18 @@
+//Git
+//Version control system designed to keep chronological timeline of all your code changes.
+
+//to traverse file system in terminal:
+//cd foldername --> moves you down
+//cd .. --> moves you up
+//ls --> lists what's inside folder
+//pwd --> tells your current directory path (print working directory)
+
+//to initiate a git repository:
+// git init
+
+//staging files for a commit:
+// git status tells you the status of your repository (the main folder you're tracking)
+// git add <file_name> OR git add . to add all of the files inside the folder you're in and stage them for a commit
+
+//how to commit:
+// git commit -m "your commit message here"
